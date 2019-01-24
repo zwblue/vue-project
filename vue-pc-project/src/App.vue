066eea47809dedb1d7d937bc98dd4a5611d57fe6
左旺 <zuowang@zuowangdeMacBook-Pro.local>
2019-01-24 10:42:06 +0800

    add Home 组件

--- vue-pc-project/src/App.vue
+++ vue-pc-project/src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home v-if='isLogin'></Home>
+    <Login v-if='!isLogin' @updateIsLogin='updateIsLogin'></Login>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from './views/Home'
+import Login from './views/Login'
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    Home, Login
+  },
+  data() {
+    return {
+      isLogin: sessionStorage.getItem('isLogin')
+    }
+  },
+  methods: {
+    updateIsLogin(isLogin) {
+      this.isLogin = isLogin
+    }
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ vue-pc-project/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    Home
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped lang="scss">
+  .home{
+    background: $primary;
+  }
+</style>
